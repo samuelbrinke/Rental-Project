f0fe39e71a88c4512751861d67cd86a27c00a1d9
samuelbrinke <samuel.brinkeback@hotmail.com>
2020-11-19 14:56:19 +0100

    Added some files and designed Login page

---
+++ DatabaseConnection/Context.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+namespace DatabaseConnection
+{
+    class Context : DbContext
+    {
+
+    }
+}
---
+++ DatabaseConnection/Seeding.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseConnection
+{
+    class Seeding
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
