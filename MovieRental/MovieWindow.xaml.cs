0512ae95129780b911f1c35feedfdd21e6e16c82
JakobLarsson <jakob.larsson@live.se>
2020-11-20 10:58:58 +0100

    Added MovieWindow

---
+++ MovieRental/MovieWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MovieRental
+{
+    /// <summary>
+    /// Interaction logic for MovieWindow.xaml
+    /// </summary>
+    public partial class MovieWindow : Window
+    {
+        public MovieWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
