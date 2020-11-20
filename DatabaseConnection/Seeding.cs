7ac783ea93bda24b2911644c51fe7566060b7e55
JakobLarsson <jakob.larsson@live.se>
2020-11-20 11:17:20 +0100

    Added Seeding Users

--- DatabaseConnection/Seeding.cs
+++ DatabaseConnection/Seeding.cs
@@ -9,7 +9,17 @@ namespace DatabaseConnection
     {
         static void Main()
         {
-
+            using (var ctx = new Context())
+            {
+                ctx.AddRange(new List<User>
+                {
+                    new User {FirstName = "Gustav", LastName = "Gustavsson", Adress = "Storgatan 1", PersonalNumber = 950101, PhoneNumber = 0708223344, ZipCode = 56249 },
+                    new User {FirstName = "Oskar", LastName = "Oskarsson", Adress = "Lillgatan 1", PersonalNumber = 920201, PhoneNumber = 0708554433, ZipCode = 56248 },
+                    new User {FirstName = "Bengt", LastName = "Bengtsson", Adress = "Smedjegatan 1", PersonalNumber = 860101, PhoneNumber = 0708223399, ZipCode = 56249 }
+                });
+                ctx.SaveChanges();
+                
+            }
         }
     }
 }
