41e1c28413dd70066ab935d58a05c309d46fd36d
JakobLarsson <jakob.larsson@live.se>
2020-11-20 10:24:48 +0100

    DataBaseFirstModel&Context

--- DatabaseConnection/Context.cs
+++ DatabaseConnection/Context.cs
@@ -6,6 +6,23 @@ namespace DatabaseConnection
 {
     class Context : DbContext
     {
+        public DbSet<User> Users { get; set; }
 
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Rental> Rentals { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .LogTo(s => System.Diagnostics.Debug.WriteLine(s))
+                .UseSqlServer(@"server=.\SQLEXPRESS;" + @"database=BlockBusterDB;" + @"trusted_connection=true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
     }
 }
--- DatabaseConnection/Model.cs
+++ DatabaseConnection/Model.cs
@@ -4,7 +4,48 @@ using System.Text;
 
 namespace DatabaseConnection
 {
-    class Model
+    public class User
     {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int PersonalNumber { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public int ZipCode { get; set; }
+
+        public string Adress { get; set; }
+
+        public virtual List<Rental> Sales { get; set; }
+
+
+    }
+
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public virtual List<Rental> Sales { get; set; }
+
+
+    }
+
+    public class Rental
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual Movie Movie { get; set; }
     }
 }
