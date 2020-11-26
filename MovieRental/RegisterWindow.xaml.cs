10c5640a763b87d9832986313cc6886ef50d0f33
JakobLarsson <jakob.larsson@live.se>
2020-11-26 11:26:34 +0100

    Added Design and Function to registerWindow and addUser Method in API class

--- MovieRental/RegisterWindow.xaml.cs
+++ MovieRental/RegisterWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,6 +10,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DatabaseConnection;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MovieRental
 {
@@ -20,6 +23,17 @@ namespace MovieRental
         public RegisterWindow()
         {
             InitializeComponent();
+        }
+
+        private void Register_btn(object sender, RoutedEventArgs e)
+        {
+            API.AddUser(Username_txtBox.Text, FirstName_txtBox.Text, LastName_txtBox.Text, Convert.ToInt32(PersonalNumber_txtBox.Text), Convert.ToInt32(PhoneNumber_txtBox.Text), Convert.ToInt32(ZipCode_txtBox.Text), Adress_txtBox.Text);
+        }
+
+        private void PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
