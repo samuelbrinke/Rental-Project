e68e1829afa8173b5a616e03a6c931b4d6183369
samuelbrinke <samuel.brinkeback@hotmail.com>
2020-11-26 13:52:13 +0100

    Added password on users.

--- DatabaseConnection/Seeding.cs
+++ DatabaseConnection/Seeding.cs
@@ -35,8 +35,8 @@ namespace DatabaseConnection
                 ctx.AddRange(new List<User>
                 {
                     new User {UserName = "Gustavsson1",FirstName = "Gustav", LastName = "Gustavsson", Adress = "Storgatan 1", PersonalNumber = 950101, PhoneNumber = 0708223344, ZipCode = 56249 },
-                    new User {UserName = "Oskar2", FirstName = "Oskar", LastName = "Oskarsson", Adress = "Lillgatan 1", PersonalNumber = 920201, PhoneNumber = 0708554433, ZipCode = 56248 },
-                    new User {UserName = "Bengan", FirstName = "Bengt", LastName = "Bengtsson", Adress = "Smedjegatan 1", PersonalNumber = 860101, PhoneNumber = 0708223399, ZipCode = 56249 }
+                    new User {UserName = "Oskar2", Password = "123hej", FirstName = "Oskar", LastName = "Oskarsson", Adress = "Lillgatan 1", PersonalNumber = 920201, PhoneNumber = 0708554433, ZipCode = 56248 },
+                    new User {UserName = "Bengan", Password = "hej123", FirstName = "Bengt", LastName = "Bengtsson", Adress = "Smedjegatan 1", PersonalNumber = 860101, PhoneNumber = 0708223399, ZipCode = 56249 }
                 });
                 ctx.SaveChanges();
                 
