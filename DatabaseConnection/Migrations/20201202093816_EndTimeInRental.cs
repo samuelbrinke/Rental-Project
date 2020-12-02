f4649388e00493a52b77abdb45ec0cd9e7642966
JakobLarsson <jakob.larsson@live.se>
2020-12-02 10:40:54 +0100

    Added DateEnd To Model.Rental and linked it with API rental

---
+++ DatabaseConnection/Migrations/20201202093816_EndTimeInRental.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatabaseConnection.Migrations
+{
+    public partial class EndTimeInRental : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Rentals",
+                newName: "DateStart");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateEnd",
+                table: "Rentals",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateEnd",
+                table: "Rentals");
+
+            migrationBuilder.RenameColumn(
+                name: "DateStart",
+                table: "Rentals",
+                newName: "Date");
+        }
+    }
+}
--- DatabaseConnection/Model.cs
+++ DatabaseConnection/Model.cs
@@ -44,7 +44,9 @@ namespace DatabaseConnection
     {
         public int Id { get; set; }
 
-        public DateTime Date { get; set; }
+        public DateTime DateStart { get; set; }
+
+        public DateTime DateEnd { get; set; }
 
         public virtual User User { get; set; }
 
